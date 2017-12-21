67d8c533460d12c20c2cfb8645a96de4446c3303
Jan <jan.bohinec@gmail.com>
2017-12-21 19:29:47 +0100

    day2
    Coding for AoC 2017 Day2

---
+++ AdventOfCode/day2.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  7 20:56:38 2017
+
+@author: Jan
+"""
+
+import time
+## Advent of Code 2017, Day 2
+        
+
+
+
+
+    
+    
+t2 = time.time()
+print('Program run for  {0} sec.'.format(round(t2-t1,2)))
+
