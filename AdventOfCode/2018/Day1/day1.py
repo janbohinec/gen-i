9f7e3f525130e2124f37e55d7374506256c71ba5
Jan <jan.bohinec@gmail.com>
2018-11-30 20:44:07 +0100

    2018 prep

---
+++ AdventOfCode/2018/Day1/day1.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 30 20:42:22 2018
+
+@author: Jan
+"""
+
+
+import numpy as np
+import pandas as pd
+import time
+import datetime as dt
+
+## Advent of Code 2018, Day 11
+day = 1
+
+
+
+if __name__ == '__main__':
+    t1 = time.time()
+    print("***  It's Advent of Code 2018, Day {}!  ***\n".format(day))
+    print('First part:')
+
+
+    
+    print('Second part:')
+
+    
+    t2 = time.time()
+    print('Program run for  {0} sec.'.format(round(t2-t1,2)))
+
--- AdventOfCode/2018/README 2018.txt
+++ AdventOfCode/2018/README 2018.txt
@@ -1,2 +1,2 @@
-Advent of Code 2018 
+### Advent of Code 2018 
 https://adventofcode.com/2018
