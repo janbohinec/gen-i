8510d20d25df732217bfd7451a1bc14647043a6b
Jan <jan.bohinec@gmail.com>
2018-12-01 06:24:34 +0100

    day1

--- AdventOfCode/2018/Day1/day1.py
+++ AdventOfCode/2018/Day1/day1.py
@@ -11,20 +11,47 @@ import pandas as pd
 import time
 import datetime as dt
 
-## Advent of Code 2018, Day 11
+## Advent of Code 2018, Day 1
 day = 1
 
 
 
 if __name__ == '__main__':
+    
     t1 = time.time()
     print("***  It's Advent of Code 2018, Day {}!  ***\n".format(day))
     print('First part:')
 
+    freq = 0
+    file_obj = open('day1.txt', 'r')
+    
+    dict_freq = [0]
+    for line in file_obj:
+        freq += int(line)
 
+    print(freq)
     
     print('Second part:')
 
+    freq = 0
+    found = False
+    file_obj = open('day1.txt', 'r')
+    
+    dict_freq = {0: 1}
+    
+    while not found:
+        
+        file_obj = open('day1.txt', 'r')
+        for line in file_obj:
+            freq += int(line)
+            if freq in dict_freq:
+                print(freq)
+                found = True
+                break
+            else:
+                dict_freq[freq] = 1
+        if found:
+            break
     
     t2 = time.time()
     print('Program run for  {0} sec.'.format(round(t2-t1,2)))
