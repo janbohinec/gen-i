26216c6d3c4cc2414ca4413ccb90466934b22c7e
CORP\janb <jan.bohinec@gen-i.si>
2018-12-04 08:06:07 +0100

    day 4

--- AdventOfCode/2018/Day4/day4.py
+++ AdventOfCode/2018/Day4/day4.py
@@ -10,10 +10,61 @@ import numpy as np
 import pandas as pd
 import time
 import datetime as dt
+from collections import defaultdict
+import operator
 
 ## Advent of Code 2018, Day 4
 day = 4
 
+def part1(data):
+    guard_id = 0
+    guards = defaultdict(int)
+    max_sleep = defaultdict(lambda: defaultdict(int))
+    for line in sorted(data):
+        
+        elements = line.split(' ')
+        print(elements)
+        if elements[3].startswith('#'):
+            guard_id = int(elements[3][1:])
+            wake_up = int(line.split(':')[-1][:2])
+        if 'wakes up' in line:
+            wake_up = int(line.split(':')[-1][:2])
+            duration = wake_up - sleep
+            guards[guard_id] += duration
+            for i in range(sleep, wake_up):
+                max_sleep[guard_id][i] += 1
+        if 'falls asleep' in line:
+            sleep = int(line.split(':')[-1][:2])
+    print(max_sleep)
+    print(guards)
+    chosen_id = max(guards.items(), key=operator.itemgetter(1))[0]
+    return chosen_id*(max(max_sleep[chosen_id].items(), key=operator.itemgetter(1))[0])
+
+
+def part2(data):
+    cur_id = 0
+    guards = defaultdict(int)
+    max_sleep = defaultdict(lambda: defaultdict(int))
+    for line in sorted(data):
+        elements = line.split(' ')
+        if elements[3].startswith('#'):
+            cur_id = int(elements[3][1:])
+            wake_up = int(line.split(':')[-1][:2])
+        if "wakes up" in line:
+            wake_up = int(line.split(':')[-1][:2])
+            duration = wake_up - sleep
+            guards[cur_id] += duration
+            for i in range(sleep, wake_up):
+                max_sleep[cur_id][i] += 1
+        if "falls asleep" in line:
+            sleep = int(line.split(':')[-1][:2])
+    total_max = 0
+    for cur_id, el in max_sleep.items():
+        new_max = max(el.items(), key=operator.itemgetter(1))[1]
+        if new_max > total_max:
+            chosen_id = cur_id
+            total_max = new_max
+    return chosen_id*max(max_sleep[chosen_id].items(), key=operator.itemgetter(1))[0]
 
 if __name__ == '__main__':
     
@@ -21,22 +72,68 @@ if __name__ == '__main__':
     print("***  It's Advent of Code 2018, Day {0}!  ***\n".format(day))
     print('** First part:')
     
-    with open('day4.txt', 'r') as f:
-        data = [line.strip() for line in f.readlines()]
+    #with open('day4.txt', 'r') as f:
+    #    data = [line.strip() for line in f.readlines()]
+        
+    data = open('day4.txt', 'r')
+    data = data.readlines()        
 
-    res = 0
-   
-    #string = data.readline()
-    #data = pd.read_excel('day2.xlsx', header=None)  
-    #for idx, row in data.iterrows():    
-        #print(row.loc[1], max(row), min(row))
+    guard_id = 0
+    guards = defaultdict(int)
+    max_sleep = defaultdict(lambda: defaultdict(int))
+    for line in sorted(data):
+        
+        elements = line.split(' ')
+        #print(elements)
+        if elements[3].startswith('#'):
+            guard_id = int(elements[3][1:])
+            wake_up = int(line.split(':')[-1][:2])
+        if 'wakes up' in line:
+            wake_up = int(line.split(':')[-1][:2])
+            duration = wake_up - sleep
+            guards[guard_id] += duration
+            for i in range(sleep, wake_up):
+                max_sleep[guard_id][i] += 1
+        if 'falls asleep' in line:
+            sleep = int(line.split(':')[-1][:2])
+    #print(max_sleep)
+    #print(guards)
     
+    chosen_id = max(guards.items(), key=operator.itemgetter(1))[0]
+    res =  chosen_id * (max(max_sleep[chosen_id].items(), key=operator.itemgetter(1))[0])
+
+    
+
     print('Silver star answer: \n{0}'.format(res))
     
     print('** Second part:')
     
-
-
+    guard_id = 0
+    guards = defaultdict(int)
+    max_sleep = defaultdict(lambda: defaultdict(int))
+    for line in sorted(data):
+        
+        elements = line.split(' ')
+        #print(elements)
+        if elements[3].startswith('#'):
+            guard_id = int(elements[3][1:])
+            wake_up = int(line.split(':')[-1][:2])
+        if 'wakes up' in line:
+            wake_up = int(line.split(':')[-1][:2])
+            duration = wake_up - sleep
+            guards[guard_id] += duration
+            for i in range(sleep, wake_up):
+                max_sleep[guard_id][i] += 1
+        if 'falls asleep' in line:
+            sleep = int(line.split(':')[-1][:2])
+    
+    total_max = 0
+    for cur_id, el in max_sleep.items():
+        new_max = max(el.items(), key=operator.itemgetter(1))[1]
+        if new_max > total_max:
+            chosen_id = cur_id
+            total_max = new_max
+    res =  chosen_id * max(max_sleep[chosen_id].items(), key=operator.itemgetter(1))[0]
     
     print('Golden star answer: \n{0}'.format(res))
     t2 = time.time()
