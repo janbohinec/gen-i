0012aaa328ff2b37ab80ddc471dcf382d5c70722
Jan <jan.bohinec@gmail.com>
2018-12-08 12:22:14 +0100

    day 9 prep

---
+++ AdventOfCode/2018/Day9/day9.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 30 20:42:22 2018
+
+@author: Jan
+"""
+
+
+import numpy as np
+import pandas as pd
+import time
+import datetime as dt
+
+## Advent of Code 2018, Day 9
+day = 9
+
+if __name__ == '__main__':
+    
+    t1 = time.time()
+    print("***  It's Advent of Code 2018, Day {0}!  ***\n".format(day))
+    print('** First part:')
+    
+    with open('day{0}.txt'.format(day), 'r') as f:
+        data = [line.strip() for line in f.readlines()]
+    
+    res = 0
+    
+
+    print('Silver star answer: \n{0}'.format(res))
+    
+    print('** Second part:')
+    
+    
+    print('Golden star answer: \n{0}'.format(res))
+    t2 = time.time()
+    print('Program run for  {0} sec.'.format(round(t2-t1,2)))
+
