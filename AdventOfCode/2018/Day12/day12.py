93d14e1cd11d9b7ac88254e0dbfea06e62ec6e2c
CORP\janb <jan.bohinec@gen-i.si>
2018-12-12 08:47:25 +0100

    day 12

--- AdventOfCode/2018/Day12/day12.py
+++ AdventOfCode/2018/Day12/day12.py
@@ -21,9 +21,7 @@ if __name__ == '__main__':
     print('** First part:')
     
     inState = '###.......##....#.#.#..###.##..##.....#....#.#.....##.###...###.#...###.###.#.###...#.####.##.#....#'
-    #inState = '#..#.#..##......###...###'
     state = '..' + inState + '.......'
-    #rule = 'LLCRR => N'
     
     with open('day{0}.txt'.format(day), 'r') as f:
         data = [line.strip() for line in f.readlines()]
@@ -50,22 +48,19 @@ if __name__ == '__main__':
         cnt = 0
         for k in range(len(state)):
             if state[k] == '#':
-                #print(k)
                 cnt += k - 2
         
         print(cnt)
-        res = 0
 
-    print('Silver star answer: \n{0}'.format(res))
+
+    print('Silver star answer: \n{0}'.format(cnt))
     
     print('** Second part:')
     
     generationsMax = 50000000000
     
     inState = '###.......##....#.#.#..###.##..##.....#....#.#.....##.###...###.#...###.###.#.###...#.####.##.#....#'
-    #inState = '#..#.#..##......###...###'
     state = '..' + inState + '.....'
-    #rule = 'LLCRR => N'
     
     def countPlants(state, generation):
         cnt = 0
@@ -82,27 +77,22 @@ if __name__ == '__main__':
     for generation in range(gens): # generations
         newState = '..'
         for i in range(2,len(state)-2): # string
-            #print(generation, i)
             for idx, mutation in enumerate(data):
                 if state[i-2:i+3] == mutation[0:5]:
                     newState += mutation[-1]
-                    #print(generation, idx, i,  state[i-2:i+3], mutation)
                     found = True
                     break
             if not found:
                 newState += state[i]
-        #print(generation, state, len(state))        
-        #print(generation, newState+'..')
         state = newState + '...'
         if generation > 40:
             state = state[1:]
-        #print(generation+1, countPlants(state, generation+1))
     
     #print(gens+1, countPlants(state, gens+1))
     print(generationsMax, countPlants(state, generationsMax))
    
     
-    print('Golden star answer: \n{0}'.format(res))
+    print('Golden star answer: \n{0}'.format(countPlants(state, generationsMax)))
     t2 = time.time()
     print('Program run for  {0} sec.'.format(round(t2-t1,2)))
 
